0013097de8a9b4020889e38c8959c7ba85b062a1
flow <flowbehappy@gmail.com>
2016-04-18 11:45:29 +0800

    bug fix: switchDirection

--- contrib/storage-indexr/src/main/java/org/apache/drill/exec/store/indexr/RSFilterGenerator.java
+++ contrib/storage-indexr/src/main/java/org/apache/drill/exec/store/indexr/RSFilterGenerator.java
@@ -25,7 +25,9 @@ public class RSFilterGenerator implements SqlVisitor<RCOperator> {
   }
 
   private Attr toAttr(SqlNode node) {
-    assert node instanceof SqlIdentifier;
+    if (!(node instanceof SqlIdentifier)) {
+      return null;
+    }
     SqlIdentifier identifier = (SqlIdentifier) node;
     return new Attr(identifier.names.get(identifier.names.size() - 1));
   }
@@ -73,16 +75,17 @@ public class RSFilterGenerator implements SqlVisitor<RCOperator> {
   private AttrAndValue toAttrAndValue(SqlNode n1, SqlNode n2) {
     SqlIdentifier idNode;
     SqlLiteral valueNode;
+    AttrAndValue av = new AttrAndValue();
     if (n1 instanceof SqlIdentifier && n2 instanceof SqlLiteral) {
       idNode = (SqlIdentifier) n1;
       valueNode = (SqlLiteral) n2;
     } else if (n2 instanceof SqlIdentifier && n1 instanceof SqlLiteral) {
       idNode = (SqlIdentifier) n2;
       valueNode = (SqlLiteral) n1;
+      av.switchDirection = true;
     } else {
       return null;
     }
-    AttrAndValue av = new AttrAndValue();
     av.attr = toAttr(idNode);
     LongAndString value = toValue(av.attr, valueNode);
     av.num = value.num;
@@ -101,30 +104,49 @@ public class RSFilterGenerator implements SqlVisitor<RCOperator> {
         return new Not(call.operand(0).accept(this));
       case BETWEEN: {
         Attr attr = toAttr(call.operand(0));
+        if (attr == null) {
+          return new UnknownOperator(call.toString());
+        }
         LongAndString v1 = toValue(attr, call.operand(1));
         LongAndString v2 = toValue(attr, call.operand(2));
         return new Between(attr, v1.num, v2.num, v1.str, v2.str);
       }
       case EQUALS: {
         AttrAndValue av = toAttrAndValue(call.operand(0), call.operand(1));
+        if (av == null) {
+          return new UnknownOperator(call.toString());
+        }
         return new Equal(av.attr, av.num, av.str);
       }
       case NOT_EQUALS: {
         AttrAndValue av = toAttrAndValue(call.operand(0), call.operand(1));
+        if (av == null) {
+          return new UnknownOperator(call.toString());
+        }
         return new NotEqual(av.attr, av.num, av.str);
       }
       case GREATER_THAN: {
         AttrAndValue av = toAttrAndValue(call.operand(0), call.operand(1));
-        return new Greater(av.attr, av.num, av.str);
+        if (av == null) {
+          return new UnknownOperator(call.toString());
+        }
+        Greater op = new Greater(av.attr, av.num, av.str);
+        return av.switchDirection? op.switchDirection() : op;
       }
       case GREATER_THAN_OR_EQUAL: {
         AttrAndValue av = toAttrAndValue(call.operand(0), call.operand(1));
-        return new GreaterEqual(av.attr, av.num, av.str);
+        if (av == null) {
+          return new UnknownOperator(call.toString());
+        }
+        GreaterEqual op = new GreaterEqual(av.attr, av.num, av.str);
+        return av.switchDirection? op.switchDirection() : op;
       }
       case IN: {
         Attr attr = toAttr(call.operand(0));
         SqlNode vns = call.operand(1);
-        assert vns instanceof SqlNodeList;
+        if (attr == null || !(vns instanceof SqlNodeList)) {
+          return new UnknownOperator(call.toString());
+        }
         SqlNodeList nl = (SqlNodeList) vns;
         List<LongAndString> values = Trick.mapToList(nl.getList(), n -> toValue(attr, n));
         long[] nums = new long[values.size()];
@@ -137,11 +159,19 @@ public class RSFilterGenerator implements SqlVisitor<RCOperator> {
       }
       case LESS_THAN: {
         AttrAndValue av = toAttrAndValue(call.operand(0), call.operand(1));
-        return new Less(av.attr, av.num, av.str);
+        if (av == null) {
+          return new UnknownOperator(call.toString());
+        }
+        Less op = new Less(av.attr, av.num, av.str);
+        return av.switchDirection? op.switchDirection() : op;
       }
       case LESS_THAN_OR_EQUAL: {
         AttrAndValue av = toAttrAndValue(call.operand(0), call.operand(1));
-        return new LessEqual(av.attr, av.num, av.str);
+        if (av == null) {
+          return new UnknownOperator(call.toString());
+        }
+        LessEqual op = new LessEqual(av.attr, av.num, av.str);
+        return av.switchDirection? op.switchDirection() : op;
       }
       default:
         return new UnknownOperator(call.toString());
@@ -184,15 +214,13 @@ public class RSFilterGenerator implements SqlVisitor<RCOperator> {
 
   private static class LongAndString {
     public long num;
-
     public String str;
   }
 
   private static class AttrAndValue {
     public Attr attr;
-
     public long num;
-
     public String str;
+    boolean switchDirection;
   }
 }
