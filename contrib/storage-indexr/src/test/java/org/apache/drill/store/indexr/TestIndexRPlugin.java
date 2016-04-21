49df10e862fe8711737ae36f58dcefd02efbbfe1
hsc <26854245@qq.com>
2016-04-21 13:50:22 +0800

    Test Embedded Server Instance

--- contrib/storage-indexr/src/test/java/org/apache/drill/store/indexr/TestIndexRPlugin.java
+++ contrib/storage-indexr/src/test/java/org/apache/drill/store/indexr/TestIndexRPlugin.java
@@ -6,9 +6,9 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- * <p/>
+ * <p>
  * http://www.apache.org/licenses/LICENSE-2.0
- * <p/>
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -32,10 +32,15 @@ public class TestIndexRPlugin extends BaseTestQuery {
     // 19 is the limit for drill transform in into hashjoin!
 
     test("select campaign_id, sum(clicks), sum(impressions) from indexr.campaign " +
-      "where `date` between 9 and 1000000000 and 0 < user_id " +
-      "and campaign_id in (0, 59855, 54638,55917, 11, 44, 55)  " +
-      "group by campaign_id order by sum(impressions) desc limit 10");
+            "where `date` between 9 and 1000000000 and 0 < user_id " +
+            "and campaign_id in (0, 59855, 54638,55917, 11, 44, 55)  " +
+            "group by campaign_id order by sum(impressions) desc limit 10");
     test("select campaign_id, sum(clicks), sum(impressions) from indexr.campaign where campaign_id < 59855 group by campaign_id order by sum(impressions) desc limit 10");
+  }
+
+  @Test
+  public void testEmbeddedServer() throws Exception {
+    test("select * from indexr.test_realtime limit 10");
   }
 
 }
